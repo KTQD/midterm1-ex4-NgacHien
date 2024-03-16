487fefaacb5b9e5924a7cc9890d23336b105f9f4
NgacHien <159707772+NgacHien@users.noreply.github.com>
2024-03-16 14:34:02 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,32 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
-    
+    private static int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
+    private static int sum = 0;
+    private static int max = Integer.MIN_VALUE;
 
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int number : numbers) {
+                sum += number;
+            }
+        });
 
-        Viết mã của bạn vào đây
-    
+        Thread t2 = new Thread(() -> {
+            for (int number : numbers) {
+                if (number > max) {
+                    max = number;
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Phan tu lon nhat trong mang la: " + max);
+        System.out.println("Tong cua cac phan tu trong mang la: " + sum);
     }
 }
